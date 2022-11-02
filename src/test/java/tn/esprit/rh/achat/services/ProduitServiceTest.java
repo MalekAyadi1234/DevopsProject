be12b46fa5d518a9a58bcca19be61e83ec3312e0
mallouka <malek.ayadi@esprit.tn>
2022-11-03 00:10:06 +0100

    enlever junit

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -7,20 +7,21 @@ import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.services.IProduitService;
 
 import java.util.List;
-
+/*
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+*/
 public class ProduitServiceTest {
 
     @Autowired
     IProduitService ps;
-
+/*
     @Test
     @Order(1)
     public void testRetrieveAllUsers() {
         List<Produit> listProduits = ps.retrieveAllProduits();
         Assertions.assertEquals(0, listProduits.size());
-    }
+    }*/
 
 
 }
