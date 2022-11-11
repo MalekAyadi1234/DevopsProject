ae48cc8364d0b84f3c6b4256e35b8cce5b8a38ec
MalekAyadi1234 <46970213+MalekAyadi1234@users.noreply.github.com>
2022-11-11 14:45:17 +0100

    Nex

--- src/test/java/tn/esprit/rh/achat/services/CategorieProduitTest.java
+++ src/test/java/tn/esprit/rh/achat/services/CategorieProduitTest.java
@@ -2,20 +2,15 @@ package tn.esprit.rh.achat.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
+
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
 import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,10 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import tn.esprit.rh.achat.entities.CategorieProduit;
-import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
-import tn.esprit.rh.achat.repositories.StockRepository;
-import tn.esprit.rh.achat.services.StockServiceImpl;
 
 @SpringBootTest
  class CategorieProduitTest {
