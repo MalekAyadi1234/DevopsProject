f84f21b9d8939870bcfad51d967ab601402ee9a1
MalekAyadi1234 <46970213+MalekAyadi1234@users.noreply.github.com>
2022-11-11 13:58:00 +0100

    TestCategotieProduit

--- src/main/java/tn/esprit/rh/achat/services/CategorieProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/CategorieProduitServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -41,6 +43,25 @@ public class CategorieProduitServiceImpl implements ICategorieProduitService {
 	public CategorieProduit retrieveCategorieProduit(Long id) {
 		CategorieProduit categorieProduit = categorieProduitRepository.findById(id).orElse(null);
 		return categorieProduit;
+	}
+	//retrieveStatusCategorie
+	@Override
+	public String retrieveStatusCategorie() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		Date now = new Date();
+		String msgDate = sdf.format(now);
+		String finalMessage = "";
+		String newLine = System.getProperty("line.separator");
+		List<CategorieProduit> stocksEnRouge = (List<CategorieProduit>) categorieProduitRepository.retrieveStatusCategorie();
+		for (int i = 0; i < stocksEnRouge.size(); i++) {
+			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
+					+ stocksEnRouge.get(i).getCodeCategorie() + " a une quantité de " + stocksEnRouge.get(i).getLibelleCategorie()
+					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getIdCategorieProduit()
+					+ newLine;
+
+		}
+	//	log.info(finalMessage);
+		return finalMessage;
 	}
 
 }
